dd0897118aff5709a268528177a3fa5cfac948f0
cetRide <okolacetric@gmail.com>
2020-02-02 14:50:13 +0300

    A user reply form created and can only participate in the discussion only when signed in

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,9 +19,25 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 @foreach($thread->replies as $reply)
-                   @include('threads.reply')
+                    @include('threads.reply')
                 @endforeach
             </div>
         </div>
+        @if (auth()->check())
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <form action="{{$thread->path().'/replies'}}" method="POST">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control"
+                                      placeholder="Have something to say?"></textarea>
+                        </div>
+                        <button class="btn btn-primary" type="submit">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{route('login')}}">sign in</a> to participate in this discussion</p>
+        @endif
     </div>
 @endsection
