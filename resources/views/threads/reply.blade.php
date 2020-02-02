448847656044c54a2f7e51181f6ae493e8d17c93
cetRide <okolacetric@gmail.com>
2020-02-03 02:27:48 +0300

    queries down to two using the \Barryvdh\Debugbar\ServiceProvider

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -7,7 +7,7 @@
 
                 <form method="POST" action="/replies/{{$reply->id}}/favorites" {{$reply->isFavorited() ? 'disabled': ''}}>
                     {{csrf_field() }}
-                    <button type="submit" class="btn btn-success">{{$reply->favorites()->count()}}{{str_plural('Favorite', $reply->favorites()->count())}}</button>
+                    <button type="submit" class="btn btn-success">{{$reply->favorites_count()}}{{str_plural('Favorite', $reply->favorites_count())}}</button>
                 </form>
         </div>
         <div class="card-body">
