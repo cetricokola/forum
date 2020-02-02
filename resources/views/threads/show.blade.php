52aa58866f498cf8a34c155e872f8e4539836325
cetRide <okolacetric@gmail.com>
2020-02-03 00:11:38 +0300

    meta information and pagination

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row ">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
@@ -14,18 +14,11 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
                 @foreach($thread->replies as $reply)
                     @include('threads.reply')
                 @endforeach
-            </div>
-        </div>
-        @if (auth()->check())
-            <div class="row justify-content-center">
-                <div class="col-md-8">
+                {{$replies->links()}}
+                @if (auth()->check())
                     <form action="{{$thread->path().'/replies'}}" method="POST">
                         {{csrf_field()}}
                         <div class="form-group">
@@ -34,10 +27,25 @@
                         </div>
                         <button class="btn btn-primary" type="submit">Post</button>
                     </form>
+                @else
+                    <p class="text-center">Please <a href="{{route('login')}}">sign in</a> to participate in this
+                        discussion</p>
+                @endif
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="body">
+                            <p>
+                                This thread was published {{$thread->created_at->diffForHumans()}} by <a href="#">{{$thread->creator->name}}</a>, and currently has
+                                {{$thread->replies_count}} comments.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        @else
-            <p class="text-center">Please <a href="{{route('login')}}">sign in</a> to participate in this discussion</p>
-        @endif
+        </div>
     </div>
+
 @endsection
